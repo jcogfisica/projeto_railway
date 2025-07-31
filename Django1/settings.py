3cf738cbb8707e8550d21341a0480c3333bc85ae
jcogfisica <jcogfisica@yahoo.com.br>
2025-07-31 18:55:51 -0300

    remder new

--- Django1/settings.py
+++ Django1/settings.py
@@ -109,20 +109,20 @@ WSGI_APPLICATION = 'Django1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 # DATABASES: Configura as informações de conexão com o banco de dados, como tipo, nome do banco, usuário, senha, etc.
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-# Replace the SQLite DATABASES configuration with PostgreSQL:
 # DATABASES = {
-# 'default': dj_database_url.config(
-# Replace this value with your local database's connection string.
-# default='postgresql://postgres:postgres@localhost:5432/mysite',
-# conn_max_age=600
-# )
-# }
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#        }
+#    }
+# Replace the SQLite DATABASES configuration with PostgreSQL:
+DATABASES = {
+    'default': dj_database_url.config(
+    # Replace this value with your local database's connection string.
+    default = 'postgresql://postgres:postgres@localhost:5432/mysite',
+    conn_max_age = 600
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
